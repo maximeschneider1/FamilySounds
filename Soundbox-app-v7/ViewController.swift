4f5cd950f169a1054253bb68a14da8cfc237f181
Maxime Schneider <1maximesch@gmail.com>
2019-02-11 16:44:43 -0500

    Initial Commit

---
+++ Soundbox-app-v7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Soundbox-app-v7
+//
+//  Created by Maxime Schneider on 11/02/2019.
+//  Copyright © 2019 Maxime Schneider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Soundbox-app-v7Tests/Soundbox_app_v7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Soundbox_app_v7Tests.swift
+//  Soundbox-app-v7Tests
+//
+//  Created by Maxime Schneider on 11/02/2019.
+//  Copyright © 2019 Maxime Schneider. All rights reserved.
+//
+
+import XCTest
+@testable import Soundbox_app_v7
+
+class Soundbox_app_v7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
